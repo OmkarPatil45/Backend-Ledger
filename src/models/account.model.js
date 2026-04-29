ec0aa3672300d4f2164ff41572deea997949649b
Omkar Patil <ompatil5132@gmail.com>
2026-04-29 13:20:14 +0530

    User, accoun, transaction, ledger schema defined

---
+++ src/models/account.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+const accountSchema = new mongoose.Schema({
+    user: {
+        // here we used objectId because we want to link this account with user collection
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: [ true, "Account must be associated with a user" ],
+        index: true
+    },
+
+    status: {
+        type: String,
+        enum: {
+            values: ["ACTIVE", "FROZEN", "CLOSED"],
+            message: "Status must be either ACTIVE, FROZEN, or CLOSED",
+        },
+        default: "ACTIVE"
+    },
+
+    currency: {
+        type: String,
+        required: [true, "Currency is required for account creation"],
+        default: "INR"
+    }
+},
+
+    {
+        timestamps: true
+    }
+)
+
+accountSchema.index({ user: 1, status: 1 })
+
+const accountModel = mongoose.model("account", accountSchema)
+
+module.exports = accountModel
