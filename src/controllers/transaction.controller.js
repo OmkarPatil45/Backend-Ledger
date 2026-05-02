95cc1c34379c0a00c0c40aac2d7483b002a60129
Omkar Patil <ompatil5132@gmail.com>
2026-05-02 17:55:23 +0530

    transaction control handling

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,103 @@
+const transactionModel = require("../models/transaction.model")
+const ledgerModel = require("../models/ledger.model")
+const accountModel = require("../models/account.model")
+
+/**
+ * - Create a new transaction
+ * THE 10-STEP TRANSFER FLOW:
+     * 1. Validate request
+     * 2. Validate idempotency key
+     * 3. Check account status
+     * 4. Derive sender balance from ledger
+     * 5. Create transaction (PENDING)
+     * 6. Create DEBIT ledger entry
+     * 7. Create CREDIT ledger entry
+     * 8. Mark transaction COMPLETED
+     * 9. Commit MongoDB session
+     * 10. Send email notification
+ */
+
+async function createTransaction(req, res) {
+    
+    /**
+     *  1. Validate request
+     */
+    const { fromAccount, toAccount, amount, idempotencyKey} = req.body
+
+    if(!fromAccount || !toAccount || !amount || !idempotencyKey) {
+        return res.status(400).json({
+            message: "Missing required fields: fromAccount, toAccount, amount, idempotencyKey"
+        })
+    }
+
+    // Check if transaction with the same idempotency key already exists
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+    })
+
+    // Check if both accounts exist
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    // If either account doesn't exist, return an error
+    if( !fromUserAccount || !toUserAccount) {
+        return res.status(400).json({
+            message: "Invalid account IDs"
+        })
+    }
+
+    /**
+     *   2. Validate idempotency key
+     */
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey,
+    })
+
+    if(isTransactionAlreadyExists) {
+        if (isTransactionAlreadyExists.status === "COMPLETED") {
+            return res.status(200).json({
+                message: "Transaction already completed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "PENDING") {
+            return res.status(200).json({
+                message: "Transaction is pending"
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "FAILED") {
+            return res.status(500).json({
+                message: "Transaction already failed"
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "RVERSED") {
+            return res.status(500).json({
+                message: "Transaction was reversed, please retry"
+            })
+        }
+
+    }
+
+    /**
+     * 3. Check account status
+     */
+
+    if (fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE") {
+        return res.status(400).json({
+            message: "Both accounts must be active to perform a transaction"
+        })
+    }
+
+    /**
+     * 4. Derive sender balance from ledger
+     */
+
+    const balance = await fromUserAccount.getBalance()
+
+
+}
