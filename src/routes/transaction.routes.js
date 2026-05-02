83fea08265062c71e6c5251dc3d4b9a9257cabea
Omkar Patil <ompatil5132@gmail.com>
2026-05-03 01:02:45 +0530

    transaction route created

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const authMiddleware = require("../middleware/auth.middleware")
-
+const transactionController = require("../controllers/transaction.controller")
 
 
 const transactionRoutes = Router();
@@ -10,9 +10,14 @@ const transactionRoutes = Router();
  * - Create a new transaction
  */
 
-transactionRoutes.post("/", authMiddleware.authMiddleware)
+transactionRoutes.post("/", authMiddleware.authMiddleware, transactionController.createTransaction)
 
+/**
+ * - POST /api/transactions/system/initial-funds
+ * - Create initial funds transaction from system user
+ */
 
+transactionRoutes.post("/system/initial-funds", authMiddleware.authSystemUserMiddleware, transactionController.createInitialFundsTransaction)     
 
 
 module.exports = transactionRoutes;
