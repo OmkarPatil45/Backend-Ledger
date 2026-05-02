359ae014b418e6936d63593978d5dfa23dcf431e
Omkar Patil <ompatil5132@gmail.com>
2026-05-03 01:02:04 +0530

    authentication middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -25,6 +25,36 @@ async function authMiddleware(req, res, next){
     }
 }
 
+async function authSystemUserMiddleware(req, res, next) {
+    const token = req.cookies.token || req.headers.authorization?.split(" ") [1]
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token not found",
+        })
+    } 
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findById(decoded.id).select("+systemUser") 
+        if (!user.systemUser) {
+            return res.status(403).json({
+                message: "Forbidden access, user is not a system user",
+            })
+        }
+
+        req.user = user
+        next()
+    }
+
+    catch (error) {
+        return res.status(401).json({
+            message: "Unauthorized access, invalid token",
+        })
+    }
+}
+
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authSystemUserMiddleware
 }
