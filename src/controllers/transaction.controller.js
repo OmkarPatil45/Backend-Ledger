b62969e4d077de5853e6244319a9e5490092fe89
Omkar Patil <ompatil5132@gmail.com>
2026-05-04 18:55:35 +0530

    transaction logic implemented

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -114,33 +114,33 @@ async function createTransaction(req, res) {
     const session = await mongoose.startSession()
     session.startTransaction()
 
-    const transaction = await transactionModel.create({
+    const transaction = new transactionModel({
         fromAccount,
         toAccount,
         amount,
         idempotencyKey,
         status: "PENDING"
-    }, { session })
+    })
 
     /**
      * 6. Create DEBIT ledger entry
      */
-    const debitLedgerEntry = await ledgerModel.create({
+    const debitLedgerEntry = await ledgerModel.create([ {
         account: fromAccount,
         amount: amount,
         transaction: transaction._id,
         type: "DEBIT"
-    }, { session })
+    } ], { session })
 
     /**
      * 7. Create CREDIT ledger entry
      */
-    const creditLedgerEntry = await ledgerModel.create({
+    const creditLedgerEntry = await ledgerModel.create([ {
         account: toAccount,
         amount: amount,
         transaction: transaction._id,
         type: "CREDIT"
-    }, { session})
+    } ], { session})
 
 
     /**
