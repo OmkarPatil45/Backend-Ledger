37d262909bd1200bdcdb21d99a5a608a1924c664
Omkar Patil <ompatil5132@gmail.com>
2026-05-04 16:25:14 +0530

    balance check router added

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -14,8 +14,19 @@ const router = express.Router()
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
 
 
+/**
+ * - GET /api/accounts/balance
+ * - Get account balance for the logged in user
+ * - Protected Route (Requires Authentication)
+ */
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController)
 
 
-
+/**
+ * - GET /api/accounts/balance/:accountId
+ * - Get balance for a specific account by ID
+ * - Protected Route (Requires Authentication) 
+ */
+router.get("/balance/:accountId", authMiddleware.authMiddleware, accountController.getAccountBalanceController)
 
 module.exports = router
