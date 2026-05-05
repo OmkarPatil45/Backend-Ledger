138880a5c37838ba2a237a63934497c85b3b5a79
Omkar Patil <ompatil5132@gmail.com>
2026-05-05 11:58:39 +0530

    logout functionality added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 const dotenv = require("dotenv")
 const bcrypt = require("bcryptjs")
+const tokenBlacklistModel = require("../models/accounts.model")
 
 dotenv.config()
 
@@ -94,8 +95,35 @@ async function userLoginController(req, res) {
     })
 }
 
+/**
+ * - user logout controller
+ * - POST /api/ auth/logout
+ * - This will clear the token cookie on the client side to log the user out
+ */
+async function userLogoutController(req, res) {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+    if (!token) {
+        return res.status(400).json({
+            message: "User logged out successfully"
+        })
+    }
+
+    await tokenBlacklistModel.create({
+        token: token
+    })
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+}
+
+
 module.exports = { 
     userRegisterController,
-    userLoginController
+    userLoginController,
+    userLogoutController
 }
 
